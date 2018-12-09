e301a746eb790aa44a4ac4e3b046aee51ee43b11
Anirban Bera <anirban.bera.89@gmail.com>
2018-12-10 01:49:15 +0530

    Added navbar and logo

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react'
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-Vents</h1>
-        <EventDashboard/>
+      <div>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
