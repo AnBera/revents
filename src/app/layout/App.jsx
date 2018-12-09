2c842275c1a68bf228438d85fd581e77555606fb
Anirban Bera <anirban.bera.89@gmail.com>
2018-12-09 12:09:04 +0530

    Added grid from semantic ui ract.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Re-Vents</h1>
+        <EventDashboard/>
       </div>
     );
   }
